a0c5cdf6130f91e5f19b23cf8279e39dfd5e3085
Ronny Bräunlich <ronny.braeunlich@codecentric.de>
2018-12-21 13:58:38 +0100

    make the onRequest function work in google cloud

--- pagescanner/src/main/kotlin/Main.kt
+++ pagescanner/src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-@JsName("onRequest")
+external val exports: dynamic
+
 fun onRequest(request: dynamic, response: dynamic) {
     console.log("Request headers: " + toJson(request.headers))
     console.log("Request body: " + toJson(request.body))
 
     response.send("Hello Serverless!")
+}
+
+fun main(args: Array<String>){
+    // this is necessary because cloud functions ignores module.exports. It only reads exports.
+    exports.onRequest = ::onRequest
 }
